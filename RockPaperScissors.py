598719832fab5336614859640a41ac6e3e854667
code-with-adhi <125806079+code-with-adhi@users.noreply.github.com>
2024-03-29 13:35:29 +0530

    Infinite loop is solved.
    1. Rules are added for shown for users' comfort.
    2. Not case sensitive anymore
    3. Infinite loop is solved

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,5 +1,10 @@
+#importing random module to make the program choose a random object in the list
 import random
+
+#initializing a list with valid inputs
 Keys= ['ROCK', 'PAPER','SCISSORS']
+print('***************RULES*************** \n1.You can only choose a valid input \n2.Each round contributes 1 point, player who wins a match gets 1 point \n3.If the match ends in draw nobody gets a point ')
+print('Valid moves: ', Keys)
 w=1
 while w == 1:   
     score_a = 0
@@ -8,43 +13,42 @@ while w == 1:
     CD = 0
     while (score_a<5 or score_z<5) and ((AB != 1) or (CD != 1)):
         B = str(input('Enter your choice(rock or paper or scissors): '))
-        A= B.upper()
+        A= B.upper()#using upper function so that the program won't be case sensitive
         Z = random.choice(Keys)
         if A in Keys:
             if A=='ROCK' and Z=='PAPER':
                 score_z = score_z + 1
-                #print('your choice: ', A , ' \nsystem choice: ', Z, '\n SYSTEM WINS')
                 print(f'Your Choice : {A} \nSystem Choice : {Z} \nSystem Wins')
                 print(f'Your score : {score_a} \nSystem score: {score_z}')
 
             elif A=='ROCK'and Z== 'SCISSORS':
                 score_a = score_a + 1
-                print('your choice: ', A , ' \nsystem choice: ', Z, '\n YOU WIN')
-                print('Your score: ', score_a, '\nSystem score: ', score_z)
+                print(f'Your Choice : {A} \nSystem Choice : {Z} \nYou Win')
+                print(f'Your score : {score_a} \nSystem score: {score_z}')
 
             elif A=='PAPER' and Z=='ROCK':
                 score_a = score_a + 1
-                print('your choice: ', A , ' \nsystem choice: ', Z, '\n YOU WIN')
-                print('Your score: ', score_a, '\nSystem score: ', score_z)
+                print(f'Your Choice : {A} \nSystem Choice : {Z} \nYou Win')
+                print(f'Your score : {score_a} \nSystem score: {score_z}')
 
             elif A=='PAPER' and Z == 'SCISSORS':
                 score_z = score_z + 1
-                print('your choice: ', A , ' \nsystem choice: ', Z, '\n SYSTEM WINS')
-                print('Your score: ', score_a, '\nSystem score: ', score_z)
-
+                print(f'Your Choice : {A} \nSystem Choice : {Z} \nSystem Wins')
+                print(f'Your score : {score_a} \nSystem score: {score_z}')
+            
             elif A=='SCISSORS' and Z=='PAPER':
                 score_a = score_a + 1
-                print('your choice: ', A , ' \nsystem choice: ', Z, '\n YOU WIN')
-                print('Your score: ', score_a, '\nSystem score: ', score_z)
-
+                print(f'Your Choice : {A} \nSystem Choice : {Z} \nYou Win')
+                print(f'Your score : {score_a} \nSystem score: {score_z}')
+            
             elif A == 'SCISSORS' and Z=='ROCK':
                 score_z = score_z + 1
-                print('your choice: ', A , ' \nsystem choice: ', Z, '\n SYSTEM WINS')
-                print('Your score: ', score_a, '\nSystem score: ', score_z)
-
+                print(f'Your Choice : {A} \nSystem Choice : {Z} \nSystem Wins')
+                print(f'Your score : {score_a} \nSystem score: {score_z}')
+            
             elif A == Z:
-                print('your choice: ', A , ' \nsystem choice: ', Z, '\n Draw')
-                print('Your score: ', score_a, '\nSystem score: ', score_z)
+                print(f'Your Choice : {A} \nSystem Choice : {Z} \nDraw')
+                print(f'Your score : {score_a} \nSystem score: {score_z}')
             
             if score_a == 5:
                 print('You won the match \nTHANKS FOR PLAYIING\n')
@@ -63,4 +67,3 @@ while w == 1:
         w = 1
     else:
         w = 0
-    
